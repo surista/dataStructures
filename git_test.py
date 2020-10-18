d3aa454b23b370c143871cb4a623acdcd6a3c3a1
s.urista <surista@g.harvard.edu>
2020-10-18 18:15:41 +0900

    added git_test.py

---
+++ git_test.py
@@ -0,0 +1,39 @@
+"""
+Code example from Think Python, by Allen B. Downey.
+Available from http://thinkpython.com
+
+Copyright 2012 Allen B. Downey.
+Distributed under the GNU General Public License at gnu.org/licenses/gpl.html.
+"""
+
+
+class Point(object):
+    """Represents a point in 2-D space."""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"  # - jdp
+
+    def __eq__(self, other):  # - jdp
+        return self.x == other.x and self.y == other.y
+
+
+def test():
+    one = Point(3, 4)
+
+    two = Point(3, 4)
+    print(one)
+    print(two)
+
+    print("Are they equal?", one == two)
+    two = Point(3, 5)
+    print(one)
+    print(two)
+    print("Are they equal?", one == two)
+
+
+if __name__ == '__main__':
+    test()
