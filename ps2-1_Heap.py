c19d51678e900cbf1b3ecb63fcc12ebe9b6f72b6
s.urista <surista@g.harvard.edu>
2020-10-29 13:35:02 +0900

    update wk3

--- ps2-1_Heap.py
+++ ps2-1_Heap.py
@@ -8,56 +8,68 @@ Module documentation goes here
 
 # python3
 
+class HeapBuilder:
+    def __init__(self, data=()):
+        self._swaps = []
+        self._data = list(data)
 
-def build_heap(data):
-    """Build a heap from ``data`` inplace.
+    @property
+    def swaps(self):
+        return self._swaps
 
-    Returns a sequence of swaps performed by the algorithm.
-    """
-    # The following naive implementation just sorts the given sequence
-    # using selection sort algorithm and saves the resulting sequence
-    # of swaps. This turns the given array into a heap, but in the worst
-    # case gives a quadratic number of swaps.
-    #
-    # TODO: replace by a more efficient implementation
-    swaps = []
-    for i in range(len(data)):
-        for j in range(i + 1, len(data)):
-            if data[i] > data[j]:
-                swaps.append((i, j))
-                data[i], data[j] = data[j], data[i]
-    return swaps
+    @property
+    def data(self):
+        return self._data
+
+    @staticmethod
+    def parent(i):
+        return (i - 1) // 2
+
+    @staticmethod
+    def left_child(i):
+        return 2 * i + 1
+
+    @staticmethod
+    def right_child(i):
+        return 2 * i + 2
+
+    def sift_down(self, i):
+        size = len(self.data)
+        min_index = i
+
+        l = self.left_child(i)
+        if l < size and self.data[l] < self.data[min_index]:
+            min_index = l
+
+        r = self.right_child(i)
+        if r < size and self.data[r] < self.data[min_index]:
+            min_index = r
+
+        if i != min_index:
+            self._swaps.append((i, min_index))
+            self._data[i], self._data[min_index] = self._data[min_index], self._data[i]
+            self.sift_down(min_index)
+
+    def read_data(self):
+        n = int(input())
+        self._data = [int(s) for s in input().split()]
+        assert n == len(self._data)
+
+    def write_response(self):
+        print(len(self._swaps))
+        for swap in self._swaps:
+            print(swap[0], swap[1])
+
+    def generate_swaps(self):
+        for i in range(len(self._data) // 2 - 1, -1, -1):
+            self.sift_down(i)
+
+    def solve(self):
+        self.read_data()
+        self.generate_swaps()
+        self.write_response()
 
 
-def main():
-    n = int(input())
-    data = list(map(int, input().split()))
-    assert len(data) == n
-
-    swaps = build_heap(data)
-
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-
-
-# if __name__ == "__main__":
-#     main()
-
-
-lst = [40, 60, 10, 20, 50, 30]
-for x, y in enumerate(lst):
-    print(x, 'parent:',int((x-1)/2))
-    print(x, 'left child:',int(x*2+1))
-    print(x, 'right child:',int(x*2 + 2))
-
-
-class minHeap:
-    def getParent(self, i):
-        return int((i-1)/2)
-
-    def get_leftChild(self,i):
-        return int(2*i)
-
-    def get_rightChild(self,i):
-        return int(2*i+1)
+if __name__ == '__main__':
+    heap_builder = HeapBuilder()
+    heap_builder.solve()
